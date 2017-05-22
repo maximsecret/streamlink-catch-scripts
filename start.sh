485d347d65bd426fc97e9b478a7f76a138b5ddce
maximsecret <maximsecret12@gmail.com>
2017-05-22 13:58:08 +0700

    oops1

--- start.sh
+++
@@ -1,6 +0,0 @@
-cd $HOME/Videos/StreamCatch/Leslyzerosix #Path for save video file.
-while [ : ]
-	do
-	streamlink https://picarto.tv/Leslyzerosix best -o $(date +%Y-%m-%d-time-%H_%M).mp4
-	sleep 1m
-done
