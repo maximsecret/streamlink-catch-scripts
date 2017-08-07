7bd3da7b8e7d56e3b1ccbb65ddf5d96d4384cdf1
maximsecret <maximsecret12@gmail.com>
2017-08-07 19:32:12 +0700

    upgrade linux script

--- script_linux.sh
+++ script_linux.sh
@@ -1,6 +1,9 @@
-cd $HOME/Videos/StreamCatch/Leslyzerosix #Path for save video file.
+#!/bin/bash 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+URL=https://picarto.tv/Leslyzerosix
 while [ : ]
 	do
-	streamlink https://picarto.tv/Leslyzerosix best -o $(date +%Y-%m-%d-time-%H_%M).mp4
+	streamlink $URL best -o $SCRIPTPATH/$(date +%Y-%m-%d-time-%H_%M).mp4
 	sleep 1m
 done
